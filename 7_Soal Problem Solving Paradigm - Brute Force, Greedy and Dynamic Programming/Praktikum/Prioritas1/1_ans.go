0a4e65f159d839aaeae5b62f544f0be74f1f6cae
iqbalbiondy <78798251+iqbalbiondy@users.noreply.github.com>
2023-04-01 12:44:56 +0000

    Tugas 7 Praktikum & Resume by Iqbal Biondy

---
+++ 7_Soal Problem Solving Paradigm - Brute Force, Greedy and Dynamic Programming/Praktikum/Prioritas1/1_ans.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+    "fmt"
+    "strconv"
+)
+
+func konvertDecKeBinary(n int) string {
+    if n == 0 {
+        return "0"
+    }
+    biner := ""
+    for n > 0 {
+        temp := n % 2
+        biner = strconv.Itoa(temp) + biner
+        n = n / 2
+    }
+    return biner
+}
+
+func represBiner(n int) []string {
+    ans := make([]string, n+1)
+    for i := 0; i <= n; i++ {
+        ans[i] = konvertDecKeBinary(i)
+    }
+    return ans
+}
+
+func main() {
+
+	m := 2
+    ans1 := represBiner(m)
+    fmt.Println(ans1)
+
+    n := 3
+    ans := represBiner(n)
+    fmt.Println(ans)
+
+	o := 4
+    ans2 := represBiner(o)
+    fmt.Println(ans2)
+}
